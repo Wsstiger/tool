9b715afe9b48e5a8feb4723eae649d7cee0d0965
Wsstiger <wsstiger@WsstigerdeMacBook-Pro.local>
2021-12-24 13:29:46 +0800

    add FloatDecimalPlace

--- float.go
+++ float.go
@@ -19,3 +19,14 @@ func Round(f float64, n int) float64 {
 	n10 := math.Pow10(n)
 	return math.Trunc((f+0.5/n10)*n10) / n10
 }
+
+
+//float 保留n位小数
+func FloatDecimalPlace(f float64,n int) float64{
+	DecimalPlaces := "%." + strconv.Itoa(n) + "f"
+	value,err:=strconv.ParseFloat(fmt.Sprintf(DecimalPlaces, f), 64)
+	if err!=nil{
+		fmt.Printf("保留%v小数出错,错误是%v",n,err)
+	}
+	return value
+}
